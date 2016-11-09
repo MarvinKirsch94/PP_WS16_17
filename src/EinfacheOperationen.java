319f5e17f651495f412f4b237ffbf62d10152af5
Marvin Kirsch <m-1234@live.de>
2016-11-09 15:54:13 +0100

    First time upload

---
+++ src/EinfacheOperationen.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Marvin Kirsch, Zarko Prastalo on 20.10.2016.
+ * C12
+ * Aufgabenblatt 1 - "Einfache Operationen in Java"
+ */
+public class EinfacheOperationen {
+    //Aufgabe 1
+    public int berechnen_der_naechsten_zahl(int z) {
+        int a = 1;
+        int b = 1;
+        int m = 2;
+        return (a * z + b) % m;
+    }
+    //Aufgabe 2
+    public int hashwert(char c1, char c2, char c3, char c4) {
+        int m = 5;
+        return ((int)c1 + (int)c2 + (int)c3 + (int)c4)%m;
+    }
+    //Aufgabe 3
+    public int wert_eines_flags(short flags, byte k) {
+        int b = (flags >> k)&1;
+        return b;
+    }
+    //Aufgabe 4
+    public char ascii_zeichen(char c) {
+        if((int)c + 2 > 90) {
+            c = (char)(64 + (int)c + 2 - 90);
+        } else {
+            c = (char)((int)c + 2);
+        }
+        return c;
+    }
+    //Aufgabe 5
+    public String zeichenketten(String s1, String s2) {
+        String s3 = s1 + " " + s2;
+        return s3;
+    }
+}
---
+++ src/Portnummern.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+/**
+ * Created by Marvin Kirsch on 09.11.2016.
+ * C12
+ * Aufgabenblatt 2 - "Kontrollstrukturen und Algorithmen"
+ */
+public class Portnummern {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int portnummer;
+        boolean nochmal = false;
+        do {
+            do {
+                System.out.println("Geben sie eine Portnummer ein: ");
+                portnummer = Integer.parseInt(in.readLine());
+            } while (portnummer < 0 || portnummer >= 65535);
+            //Port Art
+            String artDesPorts;
+            if (portnummer < 1024) {
+                artDesPorts = "well-known ports";
+            } else if (portnummer < 49152) {
+                artDesPorts = "registered ports";
+            } else {
+                artDesPorts = "dynamic ports";
+            }
+            System.out.println("Die Art des Ports: \n" + artDesPorts);
+            switch (portnummer) {
+                case 21:
+                    artDesPorts = "FTP (File Transfer Protocol, Uebertragung von Dateien)";
+                    break;
+                case 23:
+                    artDesPorts = "Telnet (Einloggen in entfernte Rechner)";
+                    break;
+                case 25:
+                    artDesPorts = "SMTP (Simple Mail Transfer Protocol, Mailversand)";
+                    break;
+                case 80:
+                    artDesPorts = "HTTP (HyperText Transfer Protocol, Zugriff auf Web-Server)";
+                    break;
+                case 143:
+                    artDesPorts = "IMAP (Internet Message Access Protocol, Zugriff auf Mail-Server)";
+                    break;
+                default:
+                    artDesPorts = "Sonstiger Dienst";
+                    break;
+            }
+            System.out.println(artDesPorts);
+            System.out.println("\nWollen sie das Programm von vorne starten ?(y or n");
+            String antwort = "a";
+            while(!(antwort.equalsIgnoreCase("y") || antwort.equalsIgnoreCase("n"))) {
+                System.out.println("Geben sie y für Ja ein oder n für nein: ");
+                antwort = in.readLine();
+            }
+            if(antwort.equalsIgnoreCase("y")) {
+                System.out.println("Wird neu gestarted!");
+                nochmal = true;
+            } else if(antwort.equalsIgnoreCase("n")) {
+                System.out.println("Wuensche einen schoenen Tag :)");
+                nochmal = false;
+            }
+        } while(nochmal);
+    }
+}
