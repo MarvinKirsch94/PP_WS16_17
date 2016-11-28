300848fb0d080ef55da7c6fed20943faf03be2aa
Marvin_Kirsch94 <m-1234@live.de>
2016-11-28 23:58:35 +0100

    fixed BinaereSuche

--- src/versuch_3/BinaereSuche.java
+++ src/versuch_3/BinaereSuche.java
@@ -25,25 +25,46 @@ public class BinaereSuche {
         System.out.println("Eingabe abgeschlossen.\n" +
                 "Geben sie das Wort nach dem gesucht werden soll ein: ");
         String suche = in.readLine();
+        System.out.println("Der Wert wurde an der Stelle : " + (sucheWort(feld, suche) < 0 ? "\nDas Wort konnte nicht gefunden werden :(" : sucheWort(feld, suche)));
     }
 
     public static int sucheWort(String feld[], String suche) {
-        int mitte = (feld.length - 1) / 2;
-        if(feld[mitte].equals(suche)) {
-            return mitte;
-        } else {
-            for(int x = 0; x < feld[mitte].length(); x++) {
-                if(feld[mitte].charAt(x) == suche.charAt(x)) {
-                    continue;
-                } else if(feld[mitte].charAt(x) > suche.charAt(x)) {
-                    //left
-                    for(int y = 0; y < mitte; y++) {
-                        newFeldL[y] = feld[y];
-                    }
-                } else {
-                    //right
-                    for(int z = 0; z < mitte; z++) {
-                        newFeldR[z] = feld[z];
+        int mitte;
+        int pos;
+        String newFeldL[];
+        String newFeldR[];
+        while(true) {
+            for(int i = 0; i < feld.length; i++) {
+                System.out.println(feld[i]);
+            }
+            mitte = (feld.length) / 2;
+            if (feld[mitte].equals(suche)) {
+                return mitte;
+            } else {
+                if(feld.length <= 1) return -1;
+                for (int x = 0; x < feld[mitte].length(); x++) {
+                    System.out.println("hey");
+                    if (!(feld[mitte].charAt(x) == suche.charAt(x))) {
+                        if (feld[mitte].charAt(x) > suche.charAt(x)) {
+                            //left
+                            System.out.println("left");
+                            newFeldL = new String[mitte];
+                            for (int y = 0; y < mitte; y++) {
+                                newFeldL[y] = feld[y];
+                            }
+                            feld = newFeldL;
+                            break;
+                        } else {
+                            //right
+                            System.out.println("right");
+                            newFeldR = new String[mitte];
+                            System.arraycopy(feld, mitte + 1, newFeldR, 0, feld.length - mitte);
+                            for (int z = 0; z < mitte; z++) {
+                                newFeldR[z] = feld[z + mitte + 1];
+                            }
+                            feld = newFeldR;
+                            break;
+                        }
                     }
                 }
             }
