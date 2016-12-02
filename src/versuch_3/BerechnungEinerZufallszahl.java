608bf6453e489c92a5ce7740809871e495730038
Marvin Kirsch <m-1234@live.de>
2016-12-02 22:15:24 +0100

    fixed versuch 3 vorbereitung aufgabe 1

--- src/versuch_3/BerechnungEinerZufallszahl.java
+++ src/versuch_3/BerechnungEinerZufallszahl.java
@@ -11,23 +11,25 @@ import java.io.InputStreamReader;
 public class BerechnungEinerZufallszahl {
 
     public static void main(String args[]) throws IOException {
+        int z = (int)(Math.random()*10);
+
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Geben sie einen Wert ein auf der die naechste Zufallszahl basiert.");
-        int z = Integer.parseInt(in.readLine());
+
         System.out.println("Geben sie einen Wert fuer a ein.");
         int a = Integer.parseInt(in.readLine());
         System.out.println("Geben sie einen Wert fuer b ein.");
         int b = Integer.parseInt(in.readLine());
         System.out.println("Geben sie einen Wert fuer m ein.");
         int m = Integer.parseInt(in.readLine());
-        int speicher = 0;
+
         for(int i = 0; i < 20; i++) {
-            System.out.println("Naechste Zufallszahl: " + berechnen_der_naechsten_zahl(speicher, a, b, m));
-            speicher = berechnen_der_naechsten_zahl(speicher, a, b, m);
+            System.out.println("Naechste Zufallszahl: " + berechnen_der_naechsten_zahl(z, a, b, m));
+            z = berechnen_der_naechsten_zahl(z, a, b, m);
         }
     }
 
     public static int berechnen_der_naechsten_zahl(int z, int a, int b, int m) {
+
         return (a * z + b) % m;
     }
 }
