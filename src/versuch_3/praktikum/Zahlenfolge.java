7bcb3682d7aaf722171ad504f1ad99a9e5d71c1e
Marvin Kirsch <m-1234@live.de>
2016-12-09 11:35:23 +0100

    added zahlenfolge -> einfache und doppelte schleife als methode

--- src/versuch_3/praktikum/Zahlenfolge.java
+++ src/versuch_3/praktikum/Zahlenfolge.java
@@ -7,9 +7,21 @@ package versuch_3.praktikum;
 public class Zahlenfolge {
 
     public static void main(String args[]) {
+        einfacheSchleife();
+        doppelteSchleife();
+    }
 
+    public static void einfacheSchleife() {
         for(int i = 0; i < 100; i++) {
-            System.out.println(i/10==7 || i%7==0 || i%10==7 ? "Bumm" : i );
+            System.out.println(i!=0 && (i/10==7 || i%7==0 || i%10==7) ? "Bumm" : i);
+        }
+    }
+
+    public static void doppelteSchleife() {
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.println(i!=0 && (i==7 || j==7 || (i*10+j)%7==0) ? "Bumm" : i*10+j);
+            }
         }
     }
 }
