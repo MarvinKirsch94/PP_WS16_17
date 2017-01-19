7aba6a834ae564f44ded4adfbda14cd810b39056
Marvin_Kirsch94 <m-1234@live.de>
2017-01-19 17:47:03 +0100

    testzufallszahl added

--- src/versuch_4/vorbereitung/TestZufallszahl.java
+++ src/versuch_4/vorbereitung/TestZufallszahl.java
@@ -8,5 +8,13 @@ public class TestZufallszahl {
 
     public static void main(String args[]) {
 
+        Zufallszahl z = new Zufallszahl(33, 23,7);
+        for(int i = 0; i < 10; i++) {
+            System.out.println(z.nextRand());
+            if(i == 5) {
+                z.setSeed(15);
+                z.reset();
+            }
+        }
     }
 }
